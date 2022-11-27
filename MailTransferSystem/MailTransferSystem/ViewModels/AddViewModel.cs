123019f7e389effc74bab3dffbd4115bc6d189be
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-28 01:39:33 +0400

    ViewModels added to the system.

---
+++ MailTransferSystem/MailTransferSystem/ViewModels/AddViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MailTransferSystem.Database.Models;
+
+namespace MailTransferSystem.ViewModels
+{
+    public class AddViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string From { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public Email TargetEmail { get; set; }
+
+        public string Message { get; set; }
+
+        public bool IsBulk { get; set; }
+    }
+}
---
+++ MailTransferSystem/MailTransferSystem/ViewModels/ListItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MailTransferSystem.ViewModels
+{
+    public class ListItemViewModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+        public ListItemViewModel(string from, string to, string title, string message)
+        {
+            From = from;
+            To = to;
+            Title = title;
+            Message = message;
+        }
+    }
+}
